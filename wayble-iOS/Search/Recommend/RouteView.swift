35e649ac34f70c0fc3063bcfa604f3e6a153edc4
littby <litt22@g.hongik.ac.kr>
2025-07-14 22:20:46 +0900

    💡 [Docs] 주석추가: Search 관련 파일 내부 구조 설명
    [Docs] 주석추가: Search 관련 파일 내부 구조

--- wayble-iOS/Search/Recommend/RouteView.swift
+++ wayble-iOS/Search/Recommend/RouteView.swift
@@ -16,6 +16,8 @@ struct RouteView: View {
             VStack(spacing:0) {
                 ForEach(viewModel.transportation.recommendedRoutes) { route in
                     VStack(alignment: .leading) {
+                        
+                        //소요시간, 도착 - 상태 바
                         HStack {
                             Text("\(route.totalTime)분")
                                 .font(.mainTextSemibold20)
@@ -38,8 +40,11 @@ struct RouteView: View {
                             .frame(height:24)
                         
                         ForEach(Array(route.steps.enumerated()), id: \.element.id) { idx, step in
+                            
+                            //역이나 정류장 별로 HStack 으로 묶기
                             HStack(alignment: .top, spacing: 12) {
-                                // Left: Icon + Line
+                                
+                                // 왼쪽 VStack 모음
                                 VStack(spacing: 0) {
                                     stepImage(for: step)
                                         .padding(.bottom, 4)
@@ -48,7 +53,8 @@ struct RouteView: View {
                                         .font(.mainTextSemibold12)
                                         .lineSpacing(-2)
                                         .padding(.bottom, 2)
-
+                                    
+                                    // 대중교통 밑에 선
                                     if idx < route.steps.count - 1 {
                                         let color: Color = {
                                             switch step.type {
@@ -57,14 +63,14 @@ struct RouteView: View {
                                             default: return .clear
                                             }
                                         }()
-
+                                        
                                         ZStack {
                                             
                                             Rectangle()
                                                 .fill(color)
                                                 .frame(width: 3, height: 10)
                                                 .offset(y: -10)
-
+                                            
                                             Rectangle()
                                                 .fill(color)
                                                 .frame(width: 3, height: 36)
@@ -74,14 +80,14 @@ struct RouteView: View {
                                     }
                                 }
                                 .frame(width: 50, alignment: .center)
-
-                                // Right: Station Info
+                                
+                                // 오른쪽 정보들 (VStack)
                                 VStack(alignment: .leading, spacing: 6) {
                                     VStack(alignment: .leading, spacing: 3) {
-                                        Text(step.subTitle)
+                                        Text(step.subTitle) // 상수역
                                             .font(.mainTextSemibold14)
-                                            .padding(.top, step.title.contains("홍대정문") ? 2 : (step.title.contains("도착") ? 6 : 0))
-
+                                      
+                                        //엘레베이터 유무, 시간
                                         HStack(spacing: 7) {
                                             if let detail = step.detail {
                                                 HStack(spacing: 2) {
@@ -92,47 +98,50 @@ struct RouteView: View {
                                                         .foregroundStyle(Color.blue700)
                                                 }
                                             }
-                                            
+                                         
+                                            //엘레베이터 표시랑 시간이나 정보 있으면 | 이거 표시
                                             if step.detail != nil && (step.extra != nil || step.Info != nil) {
                                                 Rectangle()
                                                     .fill(Color.gray200)
                                                     .frame(width: 1, height: 10)
                                             }
-
+                                            
                                             if let extra = step.extra {
                                                 Text(extra)
                                                     .font(.mainTextSemibold12)
                                                     .foregroundStyle(Color.error)
                                             }
-
+                                            
                                             if let info = step.Info {
                                                 Text(info)
                                                     .font(.mainTextRegular12)
                                                     .foregroundStyle(Color.gray700)
                                             }
                                         }
-                                       
+                                        
                                     }
-
+                                    
+                                    
+                                    //버스일경우
                                     HStack {
                                         if let extraBus = step.extraBus {
                                             Text(extraBus)
                                                 .font(.mainTextSemibold12)
                                                 .foregroundStyle(Color.blue700)
-
+                                            
                                             Rectangle()
                                                 .fill(Color.gray200)
                                                 .frame(width: 1, height: 10)
                                         }
-
+                                        
                                         if let busTime = step.busTime {
                                             Text(busTime)
                                                 .font(.mainTextRegular12)
                                                 .foregroundStyle(Color.gray700)
                                         }
                                     }
-
-                            
+                                    
+                                // 버스나 지하철일경우 밑줄 긋기
                                     if step.type == .subway || step.type == .bus {
                                         Rectangle()
                                             .fill(Color.gray200)
@@ -146,7 +155,7 @@ struct RouteView: View {
                     }
                     .padding(.leading, 20.0)
                     .padding(.vertical, 30.0)
-                    Rectangle()
+                    Rectangle() // 밑에 구분 선
                         .foregroundStyle(Color.clear)
                         .frame(height: 2)
                         .background(Color.gray300)
@@ -159,16 +168,17 @@ struct RouteView: View {
         }
     }
     
+    // 도착일 경우 왼쪽 이모지 "fin"으로
     private func stepImage(for step: RouteStep) -> some View {
         if step.type == .walk && step.title.contains("도착") {
             return AnyView(
                 Image("fin")
-                   
                     .frame(width: 18, height: 20)
-                    
+                
             )
         }
         
+        //각자 탈 것에 맞는 색깔로 표시 
         let color: Color = {
             switch step.type {
             case .subway:
@@ -186,7 +196,7 @@ struct RouteView: View {
                 Image("check03")
                     .offset(x:-1.2, y:2.5)
             }
-            .frame(width: 16, height: 16)
+                .frame(width: 16, height: 16)
         )
     }
 }
